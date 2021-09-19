94cda8be933408419d1cbf425f99f8e20375c9bc
AKIFUMI <awakewalker007@example.com>
2021-09-19 12:27:16 +0700

    change task background to red

--- 課題10Test/TutorialStoryBook/src/components/Task.js
+++ 課題10Test/TutorialStoryBook/src/components/Task.js
@@ -23,7 +23,7 @@ export default function Task({
           value={title}
           readOnly={true}
           placeholder="Input title"
-          style={{ textOverflow: "ellipsis" }}
+          style={{ background: 'red' }}
         />
       </div>
 
